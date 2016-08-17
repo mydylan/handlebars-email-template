86c2cb730e28839abb19ef025bba603bc737d579
code-mattclaffey <matt.claffey@codecomputerlove.com>
2016-08-17 13:53:24 +0100

    Refactor: Simplified the test.js file

--- test/test.js
+++ test/test.js
@@ -5,43 +5,36 @@ var handlebarsEmailTemplate = require('../index');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs-extra'));
 
+
+var options = {
+  root: './views/',
+  src: '_partials/',
+  dest: './test/testData/generated/',
+  srcTemplate: 'email',
+  destTemplate: 'test',
+  ext: 'hbs'
+};
+
+beforeEach(function(done) {
+  handlebarsEmailTemplate(options);
+
+  done();
+});
+
 describe('When I trigger my handlebars-email-template the output', function() {
   it('should match with the testData output.', function(done) {
-
-    var options = {
-      root: './views/',
-      src: '_partials/',
-      dest: './test/testData/generated/',
-      srcTemplate: 'email',
-      destTemplate: 'test',
-      ext: 'hbs'
-    };
-
-    var testData;
-    var results;
-
 
     fs.readFileAsync(options.dest + options.destTemplate + '.html', 'utf8').then(function(contents) {
         return contents;
     })
-    .then(function(contents) {
-      testData = contents;
+    .then(function(testData) {
+      fs.readFileAsync(options.dest + options.destTemplate + '.html', 'utf8')
 
-      return options;
-    })
-    .then(handlebarsEmailTemplate)
-    .then(function() {
-      fs.readFileAsync(options.dest + options.destTemplate + '.html', 'utf8').then(function(contents) {
-        return contents;
-      })
       .then(function(contents) {
-        results = contents;
-
-        if(testData === results) {
+        if(testData === contents) {
           done();
         } else {
           console.log('test data does not match results');
-          return true;
         }
       })
       .catch(function(e) {
